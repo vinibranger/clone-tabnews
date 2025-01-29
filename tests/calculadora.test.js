404d70172650ed07ea0f10377758d066a3b9b82d
Vinícius Branger <107061354+vinibranger@users.noreply.github.com>
2025-01-29 15:50:42 +0000

    Teste automatizado exemplo de nomenclatura

---
+++ tests/calculadora.test.js
@@ -0,0 +1,20 @@
+
+
+test("espero que 1 seja 1",()=>{
+  expect(1).toBe(1);
+})
+
+//exemplos que pode ser utilizado
+//test("teste de teste",()=>{
+// console.log("Teste 1")
+//}) 
+//---------------------------------------------//
+//test("teste de teste",function(){
+ // console.log("Esta chamando")
+//})
+//--------------------------------------------//
+//test("teste de teste",callbackFunction)
+//function callbackFunction(){
+//console.log('chamou');
+//}
+
