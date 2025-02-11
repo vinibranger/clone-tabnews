8a9d3c6a4a58452ea2653a022d1807df8b908482
Vinícius Branger <107061354+vinibranger@users.noreply.github.com>
2025-02-11 15:39:26 +0000

    add `json config`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,4 +1,4 @@
-import database from '../../../../infra/database.js'
+import database from 'infra/database.js'
 
 async function status(request,response){
   const resultado = await database.query('SELECT 10*5 as resultado;')
