cbce4ded78b776fe559b1f72bed2df21a818165e
Vinícius Branger <107061354+vinibranger@users.noreply.github.com>
2025-02-18 15:19:23 +0000

    iniciado response versao do db

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,15 @@
 import database from 'infra/database.js'
 
 async function status(request,response){
-  const resultado = await database.query('SELECT 10*5 as resultado;')
-  console.log(resultado.rows)
-  response.status(200).json({chave:"valor",teste:"1411",vini:[1111]})
+  const update_at =  new Date().toISOString();
+  const db_version_resultado = await database.query('select version()');
+
+ /// db_version =db_version_resultado.rows[0].version
+
+  response.status(200).json({
+    update_at:update_at,
+   /// db_version:db_version
+  })
 }
 
 export default status;
