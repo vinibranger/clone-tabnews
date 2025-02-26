6bc590efca77245865df51e142a8c7e2680e9127
Vinícius Branger <107061354+vinibranger@users.noreply.github.com>
2025-02-26 15:51:51 +0000

    finalizado endpoint status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,14 +1,33 @@
-import database from 'infra/database.js'
+import database from 'infra/database.js';
+
+
+const db_nome = process.env.POSTGRES_DB
 
 async function status(request,response){
-  const update_at =  new Date().toISOString();
-  const db_version_resultado = await database.query('select version()');
+  const updateAt =  new Date().toISOString();
 
- /// db_version =db_version_resultado.rows[0].version
+const databaseVersion = await database.query("SHOW server_version ;")
+const databaseVersionValue = databaseVersion.rows[0].server_version
+
+const databaseMaxConections = await database.query("SHOW max_connections;")
+const dbMaxConectionsValue = databaseMaxConections.rows[0].max_connections
+
+const databaseQtdeconexaoAtivas = await database.query({
+  text : "SELECT count(*)::int FROM pg_stat_activity where datname = $1;",
+  values : [db_nome]
+});
+const dbConections =  databaseQtdeconexaoAtivas.rows[0].count;
+
 
   response.status(200).json({
-    update_at:update_at,
-   /// db_version:db_version
+    update_at:updateAt,
+    dependencias:{
+      database:{
+        version: databaseVersionValue,
+        max_connections: parseInt(dbMaxConectionsValue),
+        qtde_connections:parseInt(dbConections)
+      }
+    }
   })
 }
 
--- tests/unit/integration/api/v1/status/get.test.js
+++ tests/unit/integration/api/v1/status/get.test.js
@@ -1,4 +1,22 @@
 test("Get to /api/v1/status espera 200", async ()=>{
   const response =  await fetch("http://localhost:3000/api/v1/status")
 expect(response.status).toBe(200)
+
+//teste data update do banco 
+const  responseBody = await response.json();
+expect (responseBody.update_at).toBeDefined();
+
+// teste version 
+const dt_convertida =  new Date(responseBody.update_at).toISOString();
+ expect(responseBody.update_at).toEqual(dt_convertida);// caso a data venha null 
+
+ // teste version do Postgres
+ expect(responseBody.dependencias.database.version).toEqual('16.7')
+
+ // Quantidade maxima de Conexão do banco
+ expect(responseBody.dependencias.database.max_connections).toEqual(100)
+
+ // Total de conexão no banco
+ expect(responseBody.dependencias.database.qtde_connections).toEqual(1)
+
 });
